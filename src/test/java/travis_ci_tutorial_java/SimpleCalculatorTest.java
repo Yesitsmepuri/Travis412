72c70966080d674f3d3c9494ec47b6549d060483
Yesitsmepuri <rpuri1@students.towson.edu>
2023-12-12 00:03:50 -0500

    Subtraction Test

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,23 +17,4 @@ public class SimpleCalculatorTest {
 	    assertEquals(2, result);
 	}
 	
-	@Test
-    public void testMultiply() {
-	SimpleCalculator calculator = new SimpleCalculator();
-        int result = calculator.multiply(3, 4);
-        assertEquals(12, result);
- }
-	@Test
-	 public void testDivide() {
-	        SimpleCalculator calculator = new SimpleCalculator();
-	        int result = calculator.divide(10, 2);
-	        assertEquals(5, result);
-	    }
-	@Test
-    public void testDivideByZero() {
-        SimpleCalculator calculator = new SimpleCalculator();
-        assertThrows(ArithmeticException.class, () -> calculator.divide(10, 0));
-    }
-	}
-
-
+	
