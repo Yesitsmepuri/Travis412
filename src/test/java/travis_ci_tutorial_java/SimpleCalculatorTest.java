b1f20a94bcf82212ca98239895e1dc820464ff7a
Yesitsmepuri <rpuri1@students.towson.edu>
2023-12-11 22:29:01 -0500

    Test for Division

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -22,6 +22,13 @@ public class SimpleCalculatorTest {
 	SimpleCalculator calculator = new SimpleCalculator();
         int result = calculator.multiply(3, 4);
         assertEquals(12, result);
-    }
-}
+ }
+	@Test
+	 public void testDivide() {
+	        SimpleCalculator calculator = new SimpleCalculator();
+	        int result = calculator.divide(10, 2);
+	        assertEquals(5, result);
+	    }
+	}
+
 
